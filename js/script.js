9c80253c73666d50a8cecea067f2cfeaa4da2f79
ATantasheva <96054339+ATantasheva@users.noreply.github.com>
2022-10-08 21:58:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,4 +27,28 @@ responsive: [
 });
 
 });
+// Перед переключением слайда, убираем классы на точках
+$('.slider').on('beforeChange', function() {
+   $('.slick-dots li').each(function(i,el){
+     $(el).removeClass('junior__dot specialist__dot director__dot');
+   })
+ })
+ // Добавляем классы после переключения слайда
+ $(".slider").on("afterChange", function (){
+   if($('.slick-slide.slick-current').hasClass('junior__background')) {
+     $('.slick-dots')
+       .addClass('junior__dot')
+       .removeClass('specialist__dot director__dot');
+   }
+   if($('.slick-slide.slick-current').hasClass('specialist__background')) {
+     $('.slick-dots')
+       .addClass('specialist__dot')
+       .removeClass('junior__dot director__dot');
+   }
+   if($('.slick-slide.slick-current').hasClass('director__background')) {
+     $('.slick-dots')
+       .addClass('director__dot')
+       .removeClass('specialist__dot junior__dot');
+   }
+ })
 
